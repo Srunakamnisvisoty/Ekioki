b4c6b7907bd6c580080e565240b6168f7d745a04
Srunakamnisvisoty <konchic27041998@gmail.com>
2022-04-05 12:07:37 +0300

    запросы к БД

--- src/main/java/game/ekivoki/config/provider/ConfigProvider.java
+++ src/main/java/game/ekivoki/config/provider/ConfigProvider.java
@@ -5,10 +5,15 @@ import game.ekivoki.config.reader.ConfigReader;
 import java.util.Map;
 
 import static game.ekivoki.constants.Constants.CONNECTION_CONF;
+import static game.ekivoki.constants.Constants.QUERY_LIST;
 
 public class ConfigProvider {
 
     public Map<String, String> getConnectionConf() {
         return new ConfigReader().getMap(CONNECTION_CONF);
+    }
+
+    public Map<String, String> queryMap() {
+        return new ConfigReader().getMap(QUERY_LIST);
     }
 }
--- src/main/java/game/ekivoki/constants/Constants.java
+++ src/main/java/game/ekivoki/constants/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 
     private static final String CONF_DIR = "conf/";
     public static final String CONNECTION_CONF = CONF_DIR + "connection.conf";
+    public static final String QUERY_LIST = CONF_DIR + "query.list";
 
     public static final String DATE_PATTERN = "dd.MM.yyyy";
     public static final String TIME_PATTERN = "HH:mm:ss";
